9bfea188d87384d918098b0d23bc447dab182133
Mizuki <62658861+xMizuki@users.noreply.github.com>
2022-09-09 18:00:59 +0200

    nerf

--- src/commands/Adventure/Assault.ts
+++ src/commands/Adventure/Assault.ts
@@ -69,8 +69,8 @@ export const execute: SlashCommand["execute"] = async (ctx: InteractionCommandCo
     let protectedNPC: NPC = {
         ...NPC
     }
-    protectedNPC.health += Math.round(userData.max_health / 7.5);
-    protectedNPC.max_health += Math.round(userData.max_health / 7.5);
+    protectedNPC.health += Math.round(userData.max_health / 10.5);
+    protectedNPC.max_health += Math.round(userData.max_health / 10.5);
 
     protectedNPC.skill_points.perception += Math.round(userData.skill_points.perception / 4);
 
